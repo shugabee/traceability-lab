ed7e9c525419a93d37e35704487a83e3fb7c1308
Shelby Webb <shelbylyn.webb@gmail.com>
2022-06-22 19:21:42 -0600

    added endpoint

--- server/server.js
+++ server/server.js
@@ -6,8 +6,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '7ee0f90790cd42bab52ad6892c8c6d88',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 app.use(express.static(path.join(__dirname, "../public")));
 
+
+app.get('/', (req, res) => {
+    rollbar.info("This is broken");
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
 
 const port = process.env.PORT || 4005;
 
